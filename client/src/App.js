1ae659566ea8c6d54419cf11e6f5da2d3b25c28c
fatma14 <fatmafadhli1@gmail.com>
2019-12-05 12:28:12 +0100

    add a map

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Map from "./components/Map";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Map />
+      </header>
+    </div>
+  );
+}
+
+export default App;
