621c8aee75ab923c00edffd0aa1039128273f7e4
fatma14 <fatmafadhli1@gmail.com>
2019-12-12 17:37:21 +0100

    deployment version three

--- server/server.js
+++ server/server.js
@@ -20,8 +20,8 @@ mongoose
   });
 
 server
-  .listen(process.env.PORT, () => {
-    console.log(`Listening on http://localhost:${process.env.PORT}`);
+  .listen(process.env.MONGODB_URI, () => {
+    console.log(`Listening on http://localhost:${process.env.MONGODB_URI}`);
   })
   .on("error", e => {
     console.log(1, e);
