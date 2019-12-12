f74dda1607eb7b01dd144bb53fd1497d5ab8b2aa
fatma14 <fatmafadhli1@gmail.com>
2019-12-12 23:45:53 +0100

    deploy

---
+++ client/build/precache-manifest.a3027f24159a519688cb438a302cece2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746101e59aecdde025aafaf7144911b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf45e672935748ae303",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5c5af0046b151f14af48",
+    "url": "/static/css/main.bf8a1bb6.chunk.css"
+  },
+  {
+    "revision": "bdf45e672935748ae303",
+    "url": "/static/js/2.ba06d329.chunk.js"
+  },
+  {
+    "revision": "5c5af0046b151f14af48",
+    "url": "/static/js/main.2f7feca9.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "c53139b599872f27babf3e08ece44450",
+    "url": "/static/media/icon.c53139b5.webp"
+  },
+  {
+    "revision": "4674720b4341ec7f5f229b4423939264",
+    "url": "/static/media/reservation.4674720b.webp"
+  },
+  {
+    "revision": "a983b0c55f57d0853e36a5f2d98d5903",
+    "url": "/static/media/search.a983b0c5.webp"
+  },
+  {
+    "revision": "d09280edd749f95a50f5f586137058cf",
+    "url": "/static/media/spices.d09280ed.jpeg"
+  },
+  {
+    "revision": "ee4c9d9735ad2a7c84b4d68c5fbb44ac",
+    "url": "/static/media/vegies.ee4c9d97.jpeg"
+  }
+]);
